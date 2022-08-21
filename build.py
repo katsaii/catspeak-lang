9168fc14aa835b307d013402e764e6044917179c
NuxiiGit <nuxii09@gmail.com>
2022-08-21 21:27:11 +0100

    added some more useful boilerplate codegeneration stuff

--- build.py
+++ build.py
@@ -1,10 +1,36 @@
+import types
+
+# Parses a file containing whitespace separated values into a list of those
+# values.
+def read_values(fileIn):
+    print("Reading file: {}".format(fileIn))
+    with open(fileIn) as file:
+        return [field for field in file.read().split()]
+
+# Updates a script file with this name.
+def write_script(fileOut, lines):
+    print("Updating file: {}".format(fileOut))
+    with open(fileOut, "w") as file:
+        file.writelines("{}\n".format(line) for line in lines)
+
+# Flattens an iterable into a single list.
+def flatten(iterable):
+    def go(target, iterable):
+        if isinstance(iterable, types.GeneratorType) \
+        or type(iterable) is list:
+            for item in iterable:
+                go(target, item)
+        else:
+            target.append(iterable)
+    out = []
+    go(out, iterable)
+    return out
 
 # Generates the boilerplate code for a schema of enum values.
-def generate_enum_stub(fileIn, fileOut, name, desc):
-    with open(fileIn) as file:
-        contents = file.read()
-    fields = [field for field in contents.split()]
+def generate_enum_stub(name, desc):
+    lowerName = name.lower()
     typeName = "Catspeak{}".format(name)
+    fields = read_values("enums/{}.tsv".format(lowerName))
     lines = []
     lines.append("//! Boilerplate for the `{}` enum.".format(typeName))
     lines.append("")
@@ -48,20 +74,11 @@ def generate_enum_stub(fileIn, fileOut, name, desc):
     lines.append("    }")
     lines.append("    return undefined;")
     lines.append("}")
-    with open(fileOut, "w") as file:
-        file.writelines("{}\n".format(line) for line in lines)
-    print("Updated file: {}".format(fileOut))
+    write_script(
+        "src/scripts/scr_catspeak_{}/scr_catspeak_{}.gml"
+                .format(lowerName, lowerName),
+        lines,
+    )
 
-generate_enum_stub(
-    "enums/token.tsv",
-    "src/scripts/scr_catspeak_token/scr_catspeak_token.gml",
-    "Token",
-    "Represents a kind of Catspeak token."
-)
-
-generate_enum_stub(
-    "enums/intcode.tsv",
-    "src/scripts/scr_catspeak_intcode/scr_catspeak_intcode.gml",
-    "Intcode",
-    "Represents a kind of Catspeak VM instruction."
-)
+generate_enum_stub("Token", "Represents a kind of Catspeak token.")
+generate_enum_stub("Intcode", "Represents a kind of Catspeak VM instruction.")
