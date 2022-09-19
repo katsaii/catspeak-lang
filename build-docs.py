7b55fc1be9b216aba7943ddc9cd7d1717562e947
NuxiiGit <nuxii09@gmail.com>
2022-09-19 20:33:52 +0100

    testing meta descriptions

--- build-docs.py
+++ build-docs.py
@@ -367,6 +367,7 @@ TEMPLATE = """
     <meta name="author" content="Kat @katsaii">
     <link rel="shortcut icon" href="./catspeak-icon.svg">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="%ABSTRACT%">
     <title>Catspeak Reference</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -448,7 +449,7 @@ TEMPLATE = """
     </div>
   </body>
 </html>
-"""
+""".replace("%ABSTRACT%", ABSTRACT)
 
 page = Page()
 page.add_section_note(
