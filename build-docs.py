675fde60d5b3f0436adee112cf92bb88f518627f
NuxiiGit <nuxii09@gmail.com>
2022-09-19 20:36:24 +0100

    whoops

--- build-docs.py
+++ build-docs.py
@@ -367,7 +367,7 @@ TEMPLATE = """
     <meta name="author" content="Kat @katsaii">
     <link rel="shortcut icon" href="./catspeak-icon.svg">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="%ABSTRACT%">
+    <meta name="description" content='%ABSTRACT%'>
     <title>Catspeak Reference</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
