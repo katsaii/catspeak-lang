98b843c56efc41de9de63cb41dbc0465d649464d
katsaii <katsaiinuxii@gmail.com>
2023-09-13 23:31:21 +0100

    nice changes

--- docs-gen/build-docs.py
+++ docs-gen/build-docs.py
@@ -1,2 +1,12 @@
 import catlog.catlog as cl
 
+meta = cl.doc.debug_document_create_example_metadata()
+book = cl.doc.debug_document_create_example()
+
+pages = cl.compile_book_pages(cl.codegen.HTMLCodegen, meta, book)
+for page in pages:
+    #print(pages)
+    pass
+
+version = f"{meta.version[0]}.{meta.version[0]}.{meta.version[0]}"
+cl.write_book_pages(f"docs/{version}", pages)
--- docs-gen/catlog/catlog.py
+++ docs-gen/catlog/catlog.py
@@ -1,14 +1,16 @@
 from . import gml, doc, codegen
+import os
 
 def compile_book_pages(codegen, meta, book):
-    return []
+    pages = [page for page in codegen.additional_pages()]
 
-def compile_example_book_pages(codegen):
-    meta = doc.debug_document_create_example_metadata()
-    book = doc.debug_document_create_example()
-    return compile_book_pages(codegen, meta, book)
+    return pages
 
 def write_book_pages(dir, pages):
     for (path, content) in pages:
-        with open(f"{dir}/{path}", "w") as file:
+        fullpath = f"{dir}/{path}"
+        fulldir = os.path.dirname(fullpath)
+        if not os.path.exists(fulldir):
+            os.makedirs(fulldir)
+        with open(fullpath, "w") as file:
             file.write(content or "whoops! nothing here! /(.0_0.)_b")
